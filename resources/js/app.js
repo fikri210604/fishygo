671ddb10171afa8406b11a355ab3d294546150b8
fikri210604 <2317051061@students.unila.ac.id>
2025-12-08 15:18:33 +0700

    solving chart.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,23 +11,8 @@ import './pagination'
 import './partials/checkout-payment';
 import './slider';
 import './profile-side';
+import './dashboard';
 
 Alpine.start();
 
-// Global Alpine store for layout (mobile sidebar)
-document.addEventListener('alpine:init', () => {
-  const persisted = (() => {
-    try { return JSON.parse(localStorage.getItem('sidebarCollapsed') || 'false'); } catch { return false }
-  })();
 
-  Alpine.store('layout', {
-    sidebarOpen: false,
-    sidebarCollapsed: persisted,
-    toggleSidebar() { this.sidebarOpen = !this.sidebarOpen },
-    toggleSidebarCollapse() {
-      this.sidebarCollapsed = !this.sidebarCollapsed;
-      try { localStorage.setItem('sidebarCollapsed', JSON.stringify(this.sidebarCollapsed)); } catch {}
-    },
-    closeSidebar() { this.sidebarOpen = false },
-  })
-})

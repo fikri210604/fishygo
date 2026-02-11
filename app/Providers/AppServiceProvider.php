9a4592b8d46e87bcc161d444c701ecf64360aa87
fikri210604 <2317051061@students.unila.ac.id>
2026-02-11 20:07:26 +0700

    benerin https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Http\Request;
 use Midtrans\Config;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,5 +29,10 @@ class AppServiceProvider extends ServiceProvider
         Config::$is3ds = config('midtrans.is_3ds');
 
         Model::preventLazyLoading(!app()->isProduction());
+
+        if (app()->isProduction()) {
+            URL::forceScheme('https');
+            request()->setTrustedProxies(request()->getClientIps(), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_AWS_ELB);
+        }
     }
 }
